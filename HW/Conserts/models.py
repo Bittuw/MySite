b1ebd683477de6aa5b17436b840c7151e6bd26a4
Bittuw <bittumworker@mail.ru>
2016-12-18 18:43:15 +0300

    HW

--- HW/Conserts/models.py
+++ HW/Conserts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 from django.contrib.auth.models import User
 
 
@@ -8,5 +9,13 @@ class Consert(models.Model):
 	theatre = models.CharField(max_length=40)
 	description = models.CharField(max_length=100)
 	time = models.DateTimeField(default = timezone.now)
-	image = models.ImageField(upload_to = 'image', default = 'Conserts/defaults/default.png')
+	image = models.ImageField(upload_to = 'image', default = 'defaults/default.png')
 	reservation = models.ManyToManyField(User)
+
+	def __str__(self):
+		return "Consert: {} {} {}".format(self.name, self.theatre, self.time)
+
+@admin.register(Consert)
+class ConsertAdmin(admin.ModelAdmin):
+	list_display = ('name', 'theatre', 'time', 'description','image')
+	search_fields = ('name',)
--- HW/Conserts/urls.py
+++ HW/Conserts/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
 	url(r'singin$', views.singin, name = 'singin' ),
 	url(r'singup$', views.singup, name = 'singup'),
-	url(r'logout$', views.logout, name = 'logout'),
-	url(r'success$', views.success, name = 'success'),
-	url(r'Conserts$', Conserts.as_view(), name = 'Conserts'),
-	url(r'Consert$', Consert.as_view(), name = 'Consert'),
-	url(r'AddConsert$', AddConsert.as_view(), name = 'AddConsert'),
+	#url(r'logout$', views.logout, name = 'logout'),
+	#url(r'success$', views.success, name = 'success'),
+	#url(r'Conserts$', Conserts.as_view(), name = 'Conserts'),
+	#url(r'Consert$', Consert.as_view(), name = 'Consert'),
+	#url(r'AddConsert$', AddConsert.as_view(), name = 'AddConsert'),
 ]
